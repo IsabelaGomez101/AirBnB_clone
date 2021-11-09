7fea19e000c307231fc4cc2a640b650a48194950
LuisaPinillos <luisapinillos20@gmail.com>
2021-11-09 17:07:29 -0500

    update

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 for other classes"""
 import uuid
 import datetime
+from models.__init__ import storage
 
 class BaseModel():
     
@@ -20,6 +21,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ Method that print a string"""
@@ -28,6 +30,7 @@ class BaseModel():
     def save(self):
         """ Updates the public instance attribute updated_at """
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ Returns a dictionary containing all keys/values of __dict__ of the instance """
