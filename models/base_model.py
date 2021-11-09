2a36f084c5f06756dc13ec9bf5e39f28c72a57cb
IsabelaGomez101 <3379@holbertonschool.com>
2021-11-09 15:22:32 -0500

    update file base model, Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -6,18 +6,31 @@ import datetime
 
 class BaseModel():
     
-    def __init__(self, id=None, created_at=None, updated_at=None):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        """ Constructor """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    pass
+                else:
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
+        """ Method that print a string"""
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id , self.__dict__))
     
     def save(self):
+        """ Updates the public instance attribute updated_at """
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
+        """ Returns a dictionary containing all keys/values of __dict__ of the instance """
         dic = self.__dict__
         dic["__class__"] = self.__class__.__name__
         dic["updated_at"] = self.updated_at.isoformat()
