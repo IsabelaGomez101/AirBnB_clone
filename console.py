e0c6468a4cf357ddfaedbd6370fd96b3f9112857
LuisaPinillos <luisapinillos20@gmail.com>
2021-11-14 20:51:51 -0500

    add emptyline function and fix EOF function

--- console.py
+++ console.py
@@ -133,11 +133,17 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, line):
         """ EOF command to exit the program """
+        print("")
         return True
 
     def do_quit(self, line):
         """ Quit command to exit the program """
         return True
+
+    def emptyline(self):
+        """ blank lines"""
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
