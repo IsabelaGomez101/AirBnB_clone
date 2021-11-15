9d73df07abee9f664f1feb54e55b4ecf5120daa2
LuisaPinillos <luisapinillos20@gmail.com>
2021-11-14 21:55:43 -0500

    test Amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" test class User"""
+""" test class Amenity"""
 import unittest
 from models.amenity import Amenity
 
 
 class TestAmenity(unittest.TestCase):
-    def test_Amenity(self):
+    def test_amenity(self):
         """ test to verify that the public class attributes exist
         and verify the required type"""
-    modelAmenity = Amenity()
+        modelamenity = Amenity()
+        self.assertIs(hasattr(Amenity, "name"), True)
+        self.assertAlmostEqual(type(modelamenity.name), str)
 
 
 if __name__ == '__main__':
